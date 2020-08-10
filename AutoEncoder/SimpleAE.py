db9bc9d3254ee59de3b6c8abd26c00953033cbf6
limon100 <mahmudurlimon41@gmail.com>
2020-08-11 00:16:28 +0600

    update with hidden layers

--- AutoEncoder/SimpleAE.py
+++ AutoEncoder/SimpleAE.py
@@ -55,6 +55,24 @@ def convert(data):
 
 training_set = convert(training_set)
 test_set = convert(test_set)
+
+
+
+training_set = torch.FloatTensor(training_set)
+test_set = torch.FloatTensor(test_set)
+
+
+
+class SAE(nn.Module):
+    
+    def __init__(self , ):
+        super(SAE , self).__init__()
         
+        self.fc1 = nn.Linear(nb_movies , 20)
+        self.fc2 = nn.Linear(20 , 10)
+        self.fc3 = nn.Linear(10 , 20)
+        self.fc4 = nn.Linear(20 , nb_movies)
+        self.activation = nn.Sigmoid()
+    
         
                      
